5faf64820f682c205fbd41d9b2474bf5567379c3
AmirSheibani <sheibani.amir@gmail.com>
2018-06-17 15:34:52 +0430

    add files
    add files

---
+++ CoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreData
+//
+//  Created by amir sheibani on 6/17/18.
+//  Copyright © 2018 amir sheibani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
